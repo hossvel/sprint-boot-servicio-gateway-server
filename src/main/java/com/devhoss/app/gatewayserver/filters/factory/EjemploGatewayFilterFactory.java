c5984ed45ee230abb6b1a228e34543a145e9b3ab
devhoss <velasco2288@gmail.com>
2023-09-11 23:36:49 -0500

    quitando el orden de la filtro factoria

--- src/main/java/com/devhoss/app/gatewayserver/filters/factory/EjemploGatewayFilterFactory.java
+++ src/main/java/com/devhoss/app/gatewayserver/filters/factory/EjemploGatewayFilterFactory.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 // import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
 import org.springframework.http.ResponseCookie;
@@ -27,7 +26,7 @@ public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<Ej
 
 	@Override
 	public GatewayFilter apply(Configuracion config) {
-		return new OrderedGatewayFilter( (exchange, chain) -> {
+		return (exchange, chain) -> {
 			
 			logger.info("ejecutando pre gateway filter factory: " + config.mensaje);
 			return chain.filter(exchange).then(Mono.fromRunnable(() -> {
@@ -39,7 +38,7 @@ public class EjemploGatewayFilterFactory extends AbstractGatewayFilterFactory<Ej
 				logger.info("ejecutando post gateway filter factory: " + config.mensaje);
 				
 			}));
-		},2);
+		};
 	}
 	
 	@Override
